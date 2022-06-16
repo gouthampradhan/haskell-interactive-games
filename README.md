3b1912fb38aafdec09fef5774a6f89d929572abc
Goutham Vidya Pradhan <goutham.vidya.pradhan@gmail.com>
2022-06-16 08:18:41 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# haskell-interactive-games
-Simple interactive games in haskell
+# Haskell Interactive Games
+Simple interactive games in haskell. 
+To play in the project directory execute the below commands 
+- `stack build`
+- `stack run`
+
+### Prerequisites
+Ensure that you have haskell and stack installed. If not please follow the instructions here: https://docs.haskellstack.org/en/stable/install_and_upgrade/
---
+++ src/Hangman.hs
@@ -0,0 +1,40 @@
+module Hangman
+    ( someFunc
+    ) where
+
+import System.IO      
+
+someFunc :: IO ()
+someFunc = do hSetBuffering stdout LineBuffering
+              putStrLn "Think of a word:"
+              word <- sgetLine
+              putStrLn "Try to guess it:"
+              play word
+
+match :: String -> String -> String
+match xs ys = [if x `elem` ys then x else '-' | x <- xs]
+
+getCh :: IO Char
+getCh = do hSetEcho stdin False
+           x <- getChar
+           hSetEcho stdin True
+           return x
+
+sgetLine :: IO String
+sgetLine = do x <- getCh
+              if x == '\n' then
+                 do putChar x
+                    return []
+              else
+                 do putChar '-'
+                    xs <- sgetLine
+                    return (x:xs)
+
+play :: String -> IO ()
+play word = do putStr "?"
+               guess <- getLine
+               if guess == word then
+                  putStrLn "You got it !!"
+               else
+                  do putStrLn (match word guess)
+                     play word
